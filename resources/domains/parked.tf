9603e87ea4ac6f1feb2dfdd318430925ea923c49
Josh McMillan <josh@joshmcmillan.co.uk>
2019-03-09 05:03:32 +0000

    Move unused domains into Terraform

---
+++ resources/domains/parked.tf
@@ -0,0 +1,59 @@
+module "mcmillan-works" {
+  source = "../../templates/parked_domain"
+  domain = "mcmillan.works"
+}
+
+module "quornhub-eu" {
+  source = "../../templates/parked_domain"
+  domain = "quornhub.eu"
+}
+
+module "feelingcagey-com" {
+  source = "../../templates/parked_domain"
+  domain = "feelingcagey.com"
+}
+
+module "wilf-dog" {
+  source = "../../templates/parked_domain"
+  domain = "wilf.dog"
+}
+
+module "chateau-technology" {
+  source = "../../templates/parked_domain"
+  domain = "chateau.technology"
+}
+
+module "burntwothings-com" {
+  source = "../../templates/parked_domain"
+  domain = "burntwothings.com"
+}
+
+module "stereo-consulting" {
+  source = "../../templates/parked_domain"
+  domain = "stereo.consulting"
+}
+
+module "stereo-engineering" {
+  source = "../../templates/parked_domain"
+  domain = "stereo.engineering"
+}
+
+module "stereo-company" {
+  source = "../../templates/parked_domain"
+  domain = "stereo.company"
+}
+
+module "stereo-wtf" {
+  source = "../../templates/parked_domain"
+  domain = "stereo.wtf"
+}
+
+module "incident-technology" {
+  source = "../../templates/parked_domain"
+  domain = "incident.technology"
+}
+
+module "incident-engineering" {
+  source = "../../templates/parked_domain"
+  domain = "incident.engineering"
+}
