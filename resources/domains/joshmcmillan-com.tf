94e17f791aecf00928c4b161deda7a1e3c947c58
Josh McMillan <josh@joshmcmillan.co.uk>
2019-09-21 06:16:54 +0100

    Add quiet subdomain to joshmcmillan.com

--- resources/domains/joshmcmillan-com.tf
+++ resources/domains/joshmcmillan-com.tf
@@ -6,3 +6,11 @@ module "joshmcmillan-com" {
 
   requires_email = true
 }
+
+resource "aws_route53_record" "quiet" {
+  name    = "quiet.joshmcmillan.com"
+  zone_id = module.joshmcmillan-com.zone_id
+  type    = "CNAME"
+  ttl     = 300
+  records = ["mcmillan-quiet.netlify.com"]
+}
