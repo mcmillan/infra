af1ecb81f3ba0e3634a2bf2a20db9516cf036b44
Josh McMillan <josh@joshmcmillan.co.uk>
2019-09-26 05:12:47 +0100

    Move .co.uk alongside all other aliases

--- resources/domains/joshmcmillan-aliases.tf
+++ resources/domains/joshmcmillan-aliases.tf
@@ -1,3 +1,13 @@
+module "joshmcmillan-co-uk" {
+  source            = "../../templates/netlify_domain"
+  domain            = "joshmcmillan.co.uk"
+  netlify_subdomain = "mcmillan-canonical-redirect"
+
+  # DO NOT EVER REMOVE BELOW LINE
+  requires_email = true # SERIOUSLY DON'T REMOVE THIS LINE OR I'LL STOP GETTING EMAIL
+  # DO NOT EVER REMOVE ABOVE LINE
+}
+
 module "joshmcmillan-net" {
   source            = "../../templates/netlify_domain"
   domain            = "joshmcmillan.net"
--- resources/domains/joshmcmillan-co-uk.tf
+++
@@ -1,9 +0,0 @@
-module "joshmcmillan-co-uk" {
-  source            = "../../templates/netlify_domain"
-  domain            = "joshmcmillan.co.uk"
-  netlify_subdomain = "mcmillan-canonical-redirect"
-
-  # DO NOT EVER REMOVE BELOW LINE
-  requires_email = true # SERIOUSLY DON'T REMOVE THIS LINE OR I'LL STOP GETTING EMAIL
-  # DO NOT EVER REMOVE ABOVE LINE
-}
