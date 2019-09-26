8f7c1b86db5003e9b7878225e0b5be4abeb328b0
Josh McMillan <josh@joshmcmillan.co.uk>
2019-09-26 04:36:50 +0100

    Acquire more joshmcmillan. TLDs

--- templates/parked_domain/main.tf
+++ templates/parked_domain/main.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_zone" "domain" {
   name    = "${var.domain}"
-  comment = "Managed by mcmillan/infra (registrar: ${var.registrar})" 
+  comment = "Managed by mcmillan/infra (registrar: ${var.registrar})"
   tags = {
     Registrar = var.registrar
   }
