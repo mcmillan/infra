c60f344899afa33f4e21cc36938d6c93297b5801
Josh McMillan <josh@joshmcmillan.co.uk>
2020-04-10 21:27:22 +0100

    Convert shush.wtf to parked domain

--- resources/domains/parked.tf
+++ resources/domains/parked.tf
@@ -82,6 +82,11 @@ module "shush-website" {
   domain = "shush.website"
 }
 
+module "shush-wtf" {
+  source = "../../templates/parked_domain"
+  domain = "shush.wtf"
+}
+
 module "stereo-company" {
   source = "../../templates/parked_domain"
   domain = "stereo.company"
--- resources/domains/shush-wtf.tf
+++
@@ -1,5 +0,0 @@
-module "shush-wtf" {
-  source            = "../../templates/netlify_domain"
-  domain            = "shush.wtf"
-  netlify_subdomain = "mcmillan-shush"
-}
